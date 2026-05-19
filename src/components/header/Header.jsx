db4d568569ac5ce015bf6bd28478f1123e171f7c
minsol-orang <minsol030507@naver.com>
2026-05-20 04:29:19 +0900

    🎉 Start: 퍼블리싱 테스트 프로젝트 시작

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import logoUrl from "../../assets/images/logo_image.svg";
+
+export default function Header() {
+
+}
---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header.jsx";
+import Footer from "../components/footer/Footer.jsx";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
